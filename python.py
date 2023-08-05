2e6eff45ed25bc522b02e93c9c2497bb008562e3
Zhentao <shizhentao@gmail.com>
2023-08-05 20:27:07 +0800

    update py to use vscode as IDE

--- python.py
+++ python.py
@@ -1,12 +1,15 @@
 # import os
 # os.chdir('D:\Dropbox\Teaching\Econ_prob_stat')
 
+# %%
+
 import pandas as pd
 import numpy as np
 
 ######## import data 
 #  remove zeros (data cleaning)
 
+# %%
 
 df = pd.read_csv('HKTop300YouTubers.csv', thousands=',')
 df = df.iloc[:, 1:6]
@@ -21,7 +24,7 @@ mask = np.all(df != 0, axis = 1)
 df2 = df.loc[mask, :]
 
 
-##
+# %%
 dflog = df2[:]
 dflog.iloc[:,1:4] = np.log(dflog.iloc[:,1:4])
 
@@ -32,7 +35,7 @@ import matplotlib.pyplot as plt
 plt.hist(df2['count'], color='green', alpha=0.5)
 plt.hist(dflog['count'], color='green', alpha=0.5)
 
-
+# %%
 ### draw ecdf
 
 import statsmodels.api as sm
@@ -77,3 +80,5 @@ dflog_subsample.to_csv('HK300_subsample.csv', index=False)
 
 
 
+
+# %%
